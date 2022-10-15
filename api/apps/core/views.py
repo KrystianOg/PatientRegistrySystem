c1c28549d49acd3b9093b180a0f25962d3fe1e53
wielorybek <63021070+wielorybek@users.noreply.github.com>
2022-10-15 18:41:53 +0200

    * install rest_framework in settings * routers * base serializers * base views

---
+++ api/apps/core/views.py
@@ -0,0 +1,32 @@
+from rest_framework import viewsets
+from .serializers import AppointmentSerializer, RequestSerializer, UserSerializer
+from .models import Appointment, Request, User
+
+
+"""
+CHEAT SHEET
+get -> list -> Queryset
+get -> retrieve -> Instance Detail
+post -> create -> New Instance
+put -> Full Update
+patch -> Partial Update (or Full)
+delete -> destroy
+"""
+
+
+class AppointmentViewSet(viewsets.ModelViewSet):
+    queryset = Appointment.objects.all()
+    serializer_class = AppointmentSerializer
+    lookup_field = 'pk'     # default
+
+
+class RequestViewSet(viewsets.ModelViewSet):
+    queryset = Request.objects.all()
+    serializer_class = RequestSerializer
+    lookup_field = 'pk'     # default
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    lookup_field = 'pk'     # default
