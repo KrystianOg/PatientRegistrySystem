c854d8e210c74ac430f23a91fa04f2e11319c95e
krasickiPawel <252749@student.pwr.edu.pl>
2022-10-16 14:46:55 +0200

    - user fixes - user-dependent views

--- api/apps/core/views.py
+++ api/apps/core/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+
 from .serializers import AppointmentSerializer, RequestSerializer, UserSerializer
 from .models import Appointment, Request, User
 
@@ -15,17 +18,50 @@ delete -> destroy
 
 
 class AppointmentViewSet(viewsets.ModelViewSet):
-    queryset = Appointment.objects.all()
     serializer_class = AppointmentSerializer
     lookup_field = 'pk'     # default
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            raise PermissionDenied
+        if user.UserType == User.UserType.PATIENT:
+            queryset = Appointment.objects.filter(patient=user)
+        elif user.UserType == User.UserType.DOCTOR:
+            queryset = Appointment.objects.filter(doctor=user)
+        else:
+            queryset = Appointment.objects.all()
+        return queryset
+
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_anonymous or user.UserType == User.UserType.PATIENT:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        super().create(request, args, kwargs)
+
 
 class RequestViewSet(viewsets.ModelViewSet):
-    queryset = Request.objects.all()
     serializer_class = RequestSerializer
     lookup_field = 'pk'     # default
 
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            raise PermissionDenied
+        if user.UserType == User.UserType.PATIENT:
+            queryset = Request.objects.filter(patient=user)
+        else:
+            queryset = Request.objects.all()
+        return queryset
 
+    def create(self, request, *args, **kwargs):
+        user = self.request.user
+        if user.is_anonymous:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        super().create(request, args, kwargs)
+
+
+# maybe without CreateModelMixin but for sure? (need to create user during signup process)
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
