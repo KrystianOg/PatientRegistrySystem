0da1f840c47fd76ac2835038765ead16b61569f1
krasickiPawel <252749@student.pwr.edu.pl>
2022-10-16 02:12:09 +0200

    tbh everyone should have access to make requests to doctor

--- api/apps/core/serializers.py
+++ api/apps/core/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Appointment, Request, User
-from rest_framework.exceptions import PermissionDenied
 
 
 class AppointmentSerializer(serializers.ModelSerializer):
@@ -23,11 +22,6 @@ class AppointmentSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(f'{value} is not a doctor.')
         return value
 
-    def validate_patient(self, value):
-        if not value.type == User.UserType.PATIENT:
-            raise serializers.ValidationError(f'{value} is not a patient.')
-        return value
-
 
 class RequestSerializer(serializers.ModelSerializer):
 
@@ -39,11 +33,6 @@ class RequestSerializer(serializers.ModelSerializer):
             'symptoms',
             'comment'
         ]
-
-    def validate_patient(self, value):
-        if not value.type == User.UserType.PATIENT:
-            raise PermissionDenied(f'{value} is not a patient.')
-        return value
 
 
 class UserSerializer(serializers.ModelSerializer):
