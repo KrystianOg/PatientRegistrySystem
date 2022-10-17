40f48d2184f9610de69aa8ccfeb2e7a4cf3e5cbe
Krystian Ogonowski <tailowskikrystian@gmail.com>
2022-10-17 04:58:54 +0200

    more black + simplify

--- api/apps/core/views.py
+++ api/apps/core/views.py
@@ -1,11 +1,13 @@
-from rest_framework import viewsets, status
-from rest_framework import exceptions
-from rest_framework.response import Response
+from rest_framework import viewsets
 
-from .serializers import AppointmentSerializer, RequestSerializer, UserSerializer, AppointmentPatientSerializer
+
+from .serializers import (
+    AppointmentSerializer,
+    RequestSerializer,
+    UserSerializer,
+)
 from .models import Appointment, Request, User
-from django.db.models import Q
-
+from .mixins import ObjectPermissionMixin
 
 """
 CHEAT SHEET
@@ -18,74 +20,24 @@ delete -> destroy
 """
 
 
-class AppointmentViewSet(viewsets.ModelViewSet):
-    lookup_field = 'pk'     # default
+class AppointmentViewSet(ObjectPermissionMixin, viewsets.ModelViewSet):
+    serializer_class = AppointmentSerializer
+    queryset = Appointment.objects.all()
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_anonymous:
-            raise exceptions.PermissionDenied
-        if user.UserType == User.UserType.PATIENT:
-            queryset = Appointment.objects.filter(patient=user)
-        elif user.UserType == User.UserType.DOCTOR:
-            queryset = Appointment.objects.filter(doctor=user)
-        else:
-            queryset = Appointment.objects.all()
-        return queryset
-
-    def get_serializer_class(self):
-        user = self.request.user
-        if user.is_anonymous:
-            raise exceptions.PermissionDenied   # will be changed in Permission issue
-        if user.UserType == User.UserType.PATIENT:
-            serializer_class = AppointmentPatientSerializer
-        else:
-            serializer_class = AppointmentSerializer
-        return serializer_class
-
-    def create(self, request, *args, **kwargs):     # redundant using separate serializer above but idk
-        user = self.request.user
-        if user.is_anonymous or user.UserType == User.UserType.PATIENT:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        super().create(request, args, kwargs)
+    def get_serializer_context(self):
+        self.request.data["doctor"] = self.request.user.pk
 
 
-class RequestViewSet(viewsets.ModelViewSet):
+class RequestViewSet(ObjectPermissionMixin, viewsets.ModelViewSet):
     serializer_class = RequestSerializer
-    lookup_field = 'pk'     # default
+    queryset = Request.objects.all()
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_anonymous:
-            raise exceptions.PermissionDenied
-        if user.UserType == User.UserType.PATIENT:
-            queryset = Request.objects.filter(patient=user)
-        else:
-            queryset = Request.objects.all()
-        return queryset
-
-    def create(self, request, *args, **kwargs):
-        user = self.request.user
-        if user.is_anonymous:
-            return Response(status=status.HTTP_403_FORBIDDEN)
-        super().create(request, args, kwargs)
+    def get_serializer_context(self):
+        self.request.data["patient"] = self.request.user.pk
+        pass
 
 
 # maybe without CreateModelMixin but for sure? (need to create user during signup process)
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
-    lookup_field = 'pk'     # default
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_anonymous:
-            raise exceptions.PermissionDenied
-        if user.UserType == User.UserType.PATIENT:
-            queryset = User.objects.get(email=user.email)
-        elif user.UserType == User.UserType.DOCTOR:
-            criterion1 = Q(type=User.UserType.PATIENT)
-            criterion2 = Q(type=User.UserType.DOCTOR)
-            queryset = User.objects.filter(criterion1 & criterion2)
-        else:
-            queryset = User.objects.all()
-        return queryset
+    queryset = User.objects.all()
