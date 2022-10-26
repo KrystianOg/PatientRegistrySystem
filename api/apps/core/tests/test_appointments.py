8f0e06dd55feb77347d8cf71541949b57cd0ad34
Krystian Ogonowski <tailowskikrystian@gmail.com>
2022-10-26 02:14:46 +0200

    kilka testów
    PS. (kilka nie przechodzi)

--- api/apps/core/tests/test_appointments.py
+++ api/apps/core/tests/test_appointments.py
@@ -6,34 +6,40 @@ from api.apps.authentication.tests.setup import TestsSetup
 from api.apps.core.models import Request
 
 
-class TestCreateRequests(TestsSetup):
+class TestCreateAppointments(TestsSetup):
     def setUp(self):
         super().setUp()
-        self.request_data = {
-            "symptoms": ["headache", "stomachache"],
-            "comment": "I have a headache and stomachache",
+        self.request = Request.objects.create(
+            patient=self.patient,
+            symptoms=["headache", "stomachache"],
+            comment="I have a headache and stomachache",
+        )
+        self.appointment_data = {
+            "request": self.request.id,
+            "date": "2020-12-12T13:40",
+            "duration": 30,
+            "patient": self.patient.id,
         }
 
-    def test_patient_can_create_request_true(self):
-
+    def test_patient_can_create_appointment_false(self):
         self.client.force_authenticate(user=self.patient)
         response = self.client.post(
-            reverse('requests-list'),
-            self.request_data,
+            reverse('appointments-list'),
+            self.appointment_data,
+            format='json',
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_doctor_can_create_appointment_true(self):
+        self.client.force_authenticate(user=self.doctor)
+        response = self.client.post(
+            reverse('appointments-list'),
+            self.appointment_data,
             format='json',
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['patient'], self.patient.id)
-
-    def test_doctor_can_create_request_true(self):
-        self.client.force_authenticate(user=self.doctor)
-        response = self.client.post(
-            reverse('requests-list'),
-            self.request_data,
-            format='json',
-        )
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['patient'], self.doctor.id)
+        self.assertEqual(response.data['doctor'], self.doctor.id)
 
 
 class TestAccessRequest(TestsSetup):
@@ -43,32 +49,32 @@ class TestAccessRequest(TestsSetup):
             email="otheruser@mail.com",
             password="12345678"
         )
-        self.other_request = Request.objects.create(
+        self.other_appointment = Request.objects.create(
             patient=self.other_user,
             symptoms=["headache", "stomachache"],
             comment="I have a headache and stomachache",
         )
 
-    def test_patient_can_read_somebody_request_false(self):
+    def test_patient_can_read_somebody_appointment_false(self):
         self.client.force_authenticate(user=self.patient)
         response = self.client.get(
-            reverse('requests-detail', kwargs={'pk': self.other_request.id}),
+            reverse('appointments-detail', kwargs={'pk': self.other_appointment.id}),
             format='json',
         )
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-    def test_patient_can_read_self_request_true(self):
+    def test_patient_can_read_self_appointment_true(self):
         self.client.force_authenticate(user=self.other_user)
         response = self.client.get(
-            reverse('requests-detail', kwargs={'pk': self.other_request.id}),
+            reverse('appointments-detail', kwargs={'pk': self.other_appointment.id}),
             format='json',
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_doctor_can_read_somebody_request_true(self):
+    def test_doctor_can_read_somebody_appointment_true(self):
         self.client.force_authenticate(user=self.doctor)
         response = self.client.get(
-            reverse('requests-detail', kwargs={'pk': self.other_request.id}),
+            reverse('appointments-detail', kwargs={'pk': self.other_appointment.id}),
             format='json',
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
